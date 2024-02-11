a02d82b4acbfb31d0f225bc81558d5f3d7c82799
Joseph Browning <j.browning527@gmail.com>
2024-02-11 16:56:48 -0500

    added inquirer questions

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
-const shape = new Triangle("Blue");
 
-console.log(shape.render());
+async function promptUser() {
+    
+    const { answers } = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'Select a shape:',
+          choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Enter the shape color:',
+        },
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter up to three characters for the logo text:',
+          validate: input => input.length <= 3,
+        },
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter the text color:',
+        },
+      ]);
+      let shape;
+      switch (answers.shape) {
+        case 'Square':
+            shape = new Square(answers.shapeColor);
+            break;
+        case 'Triangle':
+            shape = new Triangle(answers.shapeColor);
+            break;
+        case 'Circle':
+            shape = new Circle(answers.shape);
+            break;
+        default: 
+            console.error('Invalid selection');
+            break;
+      }
+    
+
+    }
