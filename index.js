94f4dd194472f710795fe3acef09394e5b7e2e87
Joseph Browning <j.browning527@gmail.com>
2024-02-11 18:03:23 -0500

    had to add xmlns property for it to show in browser right, added text coordinates to align text in center

--- index.js
+++ index.js
@@ -5,45 +5,70 @@ const { Circle, Triangle, Square } = require('./lib/shapes');
 
 async function promptUser() {
     
-    const { answers } = await inquirer.prompt([
+    const answers = await inquirer.prompt([
         {
-          type: 'list',
-          name: 'shape',
-          message: 'Select a shape:',
-          choices: ['Circle', 'Triangle', 'Square'],
+            type: 'list',
+            name: 'shapeType',
+            message: 'Select a shape:',
+            choices: ['Circle', 'Triangle', 'Square'],
         },
         {
-          type: 'input',
-          name: 'shapeColor',
-          message: 'Enter the shape color:',
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color:',
         },
         {
-          type: 'input',
-          name: 'text',
-          message: 'Enter up to three characters for the logo text:',
-          validate: input => input.length <= 3,
+            type: 'input',
+            name: 'logoText',
+            message: 'Enter up to three characters for the logo text:',
+            validate: input => input.length <= 3,
         },
         {
-          type: 'input',
-          name: 'textColor',
-          message: 'Enter the text color:',
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color:',
         },
-      ]);
-      let shape;
-      switch (answers.shape) {
-        case 'Square':
-            shape = new Square(answers.shapeColor);
-            break;
-        case 'Triangle':
-            shape = new Triangle(answers.shapeColor);
-            break;
-        case 'Circle':
-            shape = new Circle(answers.shape);
-            break;
-        default: 
-            console.error('Invalid selection');
-            break;
-      }
-    
+    ]);
+    console.log(answers);
 
+    let shape;
+    switch (answers.shapeType) {
+    case 'Square':
+        shape = new Square(answers.shapeColor, 150, 135);
+        break;
+    case 'Triangle':
+        shape = new Triangle(answers.shapeColor, 150, 135);
+        break;
+    case 'Circle':
+        shape = new Circle(answers.shapeColor, 150, 100);
+        break;
+    default: 
+        console.error('Invalid selection');
+        break;
     }
+    //create the svg based on user input in 300px x 200px container
+    const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="${shape.textPosition.x}" 
+            y="${shape.textPosition.y}" font-family="Arial" 
+            font-size="40" 
+            fill="${answers.textColor}" 
+            text-anchor="middle">
+        ${answers.logoText}
+        </text>
+        </svg>
+    `;
+
+    //write svg to file
+    fs.writeFile('logo.svg', svgContent, (err) => {
+        console.error(err);
+    })
+}
+
+async function init(){
+    promptUser();
+}
+
+// initializtion point
+init();
